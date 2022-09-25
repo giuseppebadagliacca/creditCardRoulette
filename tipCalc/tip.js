c7a67c06a884b217f95ab6d91f0be91af1442600
Giuseppe Badagliacca <joebadagliacca@gmail.com>
2022-09-25 15:35:07 -0400

    9/25/22

--- tipCalc/tip.js
+++ tipCalc/tip.js
@@ -16,7 +16,7 @@ function calculateTip(){
     const tip = (billAmountDOM * selectedPercentage).toFixed(2);
     const grandTotal = ((billAmountDOM * selectedPercentage) + billAmountDOM).toFixed(2);
 
-    display(`Bill Amount: $${bill}\nTip Amount: $${tip}\nGrand total: $${grandTotal} \nPay up!`, "winnerMsgStyle", false);
+    display(`Bill Amount: ${bill}\nTip Amount: ${tip}\nGrand total: $${grandTotal} \nPay up!`, "winnerMsgStyle", false);
   }else{
     display('Must enter both bill amount and tip percentage!', "errorMsgStyle", true);
   }
@@ -34,7 +34,15 @@ function validateInputs(){
   return (percentage && document.getElementById('bill-amount').value) ? true:false;
 }
 
-function display(msg, stlye){
+function display(msg, stlye, clear){
   displayDivDOM.className = stlye;
   displayDivDOM.innerText = msg;
+  if(clear){
+    setTimeout(clearDisplay,4000);
+  }
+}
+
+function clearDisplay(){
+  displayDivDOM.className = "";
+  displayDivDOM.innerText = "";
 }
